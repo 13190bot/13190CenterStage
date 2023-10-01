34fe7a7db976cbd50b95eae0a3ef33181c88d155
mc08 <mc08>
2023-10-01 11:37:49 -0700

    AlignTest: Made it extend BaseDriveOpMode (erm) and moved to testing teleops

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TestingTeleop/AlignTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TestingTeleop/AlignTest.java
@@ -1,15 +1,16 @@
-package org.firstinspires.ftc.teamcode.CV;
+package org.firstinspires.ftc.teamcode.TeleOp.TestingTeleop;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.controller.PIDFController;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.CV.AprilTagDetector;
 import org.firstinspires.ftc.teamcode.Commands.DriveRobotCentricCommand;
 import org.firstinspires.ftc.teamcode.TeleOp.MainTeleop.BaseOpMode;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 
 @TeleOp(name = "AlignTest")
 @Config
-public class AlignTest extends BaseOpMode {
+public class AlignTest extends BaseDriveOpMode {
 
     double forward = 0;
     double strafe = 0;
