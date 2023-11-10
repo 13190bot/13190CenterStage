143c4e840969d365e5bc47c32a2f73bec54025e0
mc08 <mc08>
2023-11-10 13:17:47 -0800

    w

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ClawSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ClawSubsystem.java
@@ -1,14 +1,17 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
+import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.command.Command;
+import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.arcrobotics.ftclib.hardware.motors.CRServo;
-
+@Config
 public class ClawSubsystem extends SubsystemBase {
 
     public static SimpleServo claw;
     public static CRServo axle;
-    private final double clawOpenPos = 0.7;
-    private final double clawClosedPos = 0.2;
+    public static double clawOpenPos = 0.7;
+    public static double clawClosedPos = 0.2;
 
 
     public ClawSubsystem(SimpleServo claw, CRServo axle) {
@@ -17,7 +20,7 @@ public class ClawSubsystem extends SubsystemBase {
     }
 
     public void grab() {
-        claw.setPosition(clawClosedPos);
+       claw.setPosition(clawClosedPos);
     }
 
     public void release() {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MainTeleop/MainOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MainTeleop/MainOpMode.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode.TeleOp.MainTeleop;
+import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.Subsystems.ArmSubsystem;
@@ -9,9 +10,10 @@ TODO:
 Implement photon https://github.com/8872/8872PP/pull/3/files
  */
 
+@Config
 @TeleOp(name = "MainTeleOp")
 public class MainOpMode extends BaseOpMode {
-
+    public static double test = 0.5;
 
     @Override
     public void initialize() {
@@ -23,8 +25,24 @@ public class MainOpMode extends BaseOpMode {
 
         gb2(PlaystationAliases.CROSS).whileHeld(startIntakeCommand);
         gb2(PlaystationAliases.TRIANGLE).toggleWhenPressed(clawGrabCommand, clawReleaseCommand);
-        gb1(GamepadKeys.Button.A).toggleWhenPressed(armSubsystem.moveArm(ArmSubsystem.armPosHome), armSubsystem.moveArm(ArmSubsystem.armPosAway));
-        gb1(GamepadKeys.Button.B).toggleWhenPressed(armSubsystem.movePitch(ArmSubsystem.pitchPosHome), armSubsystem.movePitch(ArmSubsystem.pitchPosAway));
+        // open and down
+        gb2(PlaystationAliases.CROSS).whenPressed(() -> {
+            clawSubsystem.release();
+            sleep(20);
+
+        });
+        // close and up
+
+
+        //TEST
+        gb2(PlaystationAliases.SQUARE).whenPressed(() -> {
+            armSubsystem.movePitch(test);
+
+        });
+//        gb2(PlaystationAliases)
+
+//        gb1(GamepadKeys.Button.A).toggleWhenPressed(armSubsystem.moveArm(ArmSubsystem.armPosHome), armSubsystem.moveArm(ArmSubsystem.armPosAway));
+//        gb1(GamepadKeys.Button.B).toggleWhenPressed(armSubsystem.movePitch(ArmSubsystem.pitchPosHome), armSubsystem.movePitch(ArmSubsystem.pitchPosAway));
        // clawSubsystem.setDefaultCommand(axleMoveCommand);
         //armSubsystem.setDefaultCommand(armMoveCommand);
         driveSubsystem.setDefaultCommand(driveRobotOptimalCommand);
