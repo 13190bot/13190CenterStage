547d909a85a1125ac334919f39915982ba58c8d6
mc08 <mc08>
2023-11-29 21:47:26 -0800

    RecordTeleOp fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MainTeleop/RecordTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MainTeleop/RecordTeleOp.java
@@ -8,8 +8,9 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 
-import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @TeleOp(name = "RecordTeleOp")
 public class RecordTeleOp extends LinearOpMode {
@@ -24,16 +25,56 @@ public class RecordTeleOp extends LinearOpMode {
      */
     ArrayList[] data = new ArrayList[motorNames.length + 2];
 
+
+    // Autogen / Serializer
+    // ASSUMES EVERYTHING IS DOUBLES (which it is)
+    public static String dataToCode(ArrayList[] data) {
+        String out = "ArrayList[]data={";
+        for (int i = 0; i < data.length; i++) {
+            // Optimized (https://stackoverflow.com/a/23183963)
+            out = out + "new ArrayList(Arrays.asList(" + data[i].stream().map(Object::toString).collect(Collectors.joining(",")) + "))";
+            if (i != data.length - 1) {
+                out = out + ",";
+            }
+
+
+//            ArrayList row = data[i];
+//            for (int i2 = 0; i2 < row.size(); i2++) {
+//
+//            }
+        }
+        out = out + "}";
+        return out;
+    }
+    // Test
+//    public static void main(String args[]) {
+//        System.out.println(dataToCode(
+//                new ArrayList[]{new ArrayList(Arrays.asList(0, 1, 2, 2.5))}
+//        ));
+//    }
+
     @Override
     public void runOpMode() throws InterruptedException {
+//        hardwareMap.getAll(DcMotor.class);
+
+        // TODO: Use getall to get all motors and servos
+        // Or prateek's reflection
+
         // TeleOp code here: START
         DcMotor fl = hardwareMap.get(DcMotor.class, "frontLeft");
         DcMotor fr = hardwareMap.get(DcMotor.class, "frontRight");
         DcMotor bl = hardwareMap.get(DcMotor.class, "backLeft");
         DcMotor br = hardwareMap.get(DcMotor.class, "backRight");
+        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bl.setDirection(DcMotorSimple.Direction.REVERSE);
         fl.setDirection(DcMotorSimple.Direction.REVERSE);
         br.setDirection(DcMotorSimple.Direction.REVERSE);
+        // fr and br need to be reversed for some reason
+        fl.setDirection(DcMotorSimple.Direction.REVERSE);
+        br.setDirection(DcMotorSimple.Direction.FORWARD);
         // TeleOp code here: END
 
 
@@ -52,7 +93,7 @@ public class RecordTeleOp extends LinearOpMode {
         // Recording
 
         // Wait
-        while (!gamepad1.a) {
+        while (!gamepad1.a && opModeIsActive()) {
             sleep(10);
         }
 
@@ -65,9 +106,10 @@ public class RecordTeleOp extends LinearOpMode {
             // TeleOp code here: START
             // motors[0].setPower(gamepad1.left_stick_y);
             double y = -gamepad1.left_stick_y;
-            double x = gamepad1.left_stick_x;
+            double x = gamepad1.left_stick_x * 1.1;
             double rx = gamepad1.right_trigger - gamepad1.left_trigger;
-            double denominator = Math.max(Math.abs(x) + Math.abs(y) + Math.abs(rx), 1);
+//            double denominator = Math.max(Math.abs(x) + Math.abs(y) + Math.abs(rx), 1);
+            double denominator = Math.abs(x) + Math.abs(y) + Math.abs(rx); // FORCE SPEED TO ALWAYS BE MAXED
             fl.setPower((y + x + rx) / denominator);
             bl.setPower((y - x + rx) / denominator);
             fr.setPower((y - x - rx) / denominator);
@@ -96,7 +138,7 @@ public class RecordTeleOp extends LinearOpMode {
         // Replay
 
         // Wait
-        while (!gamepad1.a) {
+        while (!gamepad1.a && opModeIsActive()) {
             sleep(10);
         }
 
@@ -129,6 +171,7 @@ public class RecordTeleOp extends LinearOpMode {
          */
 
         // Busy looping for each ms (assuming replay loop time is less than recording loop time)
+        int length = data[0].size();
         while (opModeIsActive()) {
             while ((double) data[0].get(i) > System.nanoTime() - replayingStartTime) {
                 // Busy loop
@@ -145,6 +188,9 @@ public class RecordTeleOp extends LinearOpMode {
             }
 
             i = i + 1;
+            if (i == length) {
+                break;
+            }
         }
 
     }
