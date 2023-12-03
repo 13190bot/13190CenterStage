84c1a437cb8cbf6e9c12c7a352fa700ef061f532
mc08 <mc08>
2023-12-02 18:53:55 -0800

    clean code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Recorder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Recorder.java
@@ -105,10 +105,6 @@ public final class Recorder {
         for (int i = 0; i < servos.size(); i++) {
             data[i + motorsLength + 2].add(servos.get(i).getPosition());
         }
-
-
-//        for (int i = 0;             i < motors.size();                 ++i) data[i + 2].add(motors.get(i                ).getPower());
-//        for (int i = motors.size(); i < servos.size() + motors.size(); ++i) data[i].add(servos.get(i - motors.size()).getPosition());
     }
 
     public static void saveRecording () {
@@ -131,75 +127,7 @@ public final class Recorder {
         }
     }
 
-//    public static String dataToCode () {
-//        String out = "";
-//
-//        out +=
-//                //header stuff TODO: MAKE SURE NO MORE HEADERS NECESSARY
-//                          "package org.firstinspires.ftc.teamcode.recordedAutos;"        + "\n"
-//                        + ""                                                             + "\n"
-//                        + "import com.qualcomm.robotcore.eventloop.opmode.TeleOp;"       + "\n"
-//                        + "import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;" + "\n"
-//                        + "import com.qualcomm.robotcore.hardware.*;"                    + "\n"
-//                        + ""                                                             + "\n"
-//                        + "import java.util.ArrayList"                                   + "\n"
-//                        + ""                                                             + "\n"
-//        ;
-//
-//        //code:
-//        out +=
-//                          "@Autonomous(group = \"Recorded Autos\")"                                                                 + "\n"
-//                        + "public class " + currentFile + " extends LinearOpMode {"                                                 + "\n"
-//                        + "    ArrayList<DcMotor> motors;"                                                                          + "\n"
-//                        + "    ArrayList<Servo>   servos;"                                                                          + "\n"
-//                        + ""                                                                                                        + "\n"
-//                        + "    String[] motorNames = {" + motorNames.toString().substring(1, motorNames.toString().length()) + "};" + "\n"
-//                        + "    String[] servoNames = {" + servoNames.toString().substring(1, servoNames.toString().length()) + "};" + "\n"
-//                        + ""                                                                                                        + "\n"
-//                        + "    ArrayList[] data  = {"                                                                               + "\n"
-//        ;
-//
-//        //data:
-//        for (int i = 0; i < data.length-1; ++i) out += "        (ArrayList)Arrays.asList(" + data[i].toString().substring(1, data.toString().length()-1) + "),\n";
-//        out += "        new ArrayList(Arrays.asList(" + data[data.length-1].toString().substring(1, data[data.length-1].toString().length()-1) + "))\n";
-//
-//        //continue rest of code
-//        out +=
-//                          "    };"                                                                                                                    + "\n"
-//                        + ""                                                                                                                          + "\n"
-//                        + "    //replay based off of data"                                                                                            + "\n"
-//                        + "    @Override"                                                                                                             + "\n"
-//                        + "    public void runOpMode() throws InterruptedException {"                                                                 + "\n"
-//                        + "        //get devices"                                                                                                     + "\n"
-//                        + "        motors = new ArrayList<>()"                                                                                        + "\n"
-//                        + "        servos = new Arraylist<>()"                                                                                        + "\n"
-//                        + "        for (String name : motorNames) motors.add(hardwareMap.dcMotor.get(name));"                                         + "\n"
-//                        + "        for (String name : servoNames) servos.add(hardwareMap.dcMotor.get(name));"                                         + "\n"
-//                        + ""                                                                                                                          + "\n"
-//                        + "        telemetry.addData(\"init\", \"done\");"                                                                            + "\n"
-//                        + "        telemetry.update();"                                                                                               + "\n"
-//                        + ""                                                                                                                          + "\n"
-//                        + "        waitForStart();"                                                                                                   + "\n"
-//                        + ""                                                                                                                          + "\n"
-//                        + "        //loop through the logs as long as op mode is active or untill we hit the end"                                     + "\n"
-//                        + "        double replayingStartTime = System.nanoTime();"                                                                    + "\n"
-//                        + "        for (int i = 0; opModeIsActive() && i < data[0].size(); ++i) {"                                                    + "\n"
-//                        + "            while ((double) data[0].get(i) > System.nanoTime() - replayingStartTime) {} //wait till next log"              + "\n"
-//                        + ""                                                                                                                          + "\n"
-//                        + "            //write data to devices"                                                                                       + "\n"
-//                        + "            for (int i2 = 0; i2 < motors.size(); ++i2) motors[i2].setPower((double) data[i2                 + 2].get(i));" + "\n"
-//                        + "            for (int i2 = 0; i2 < servos.size(); ++i2) servos[i2].setPower((double) data[i2 + motors.size() + 2].get(i));" + "\n"
-//                        + ""                                                                                                                          + "\n"
-//                        + "            //write to telemetry and increment targel log # (i)"                                                           + "\n"
-//                        + "            telemetry.addData(\"datapoint, i++ + \"/\" + data[0].size());"                                                 + "\n"
-//                        + "            telemetry.update();"                                                                                            + "\n"
-//                        + "        }"                                                                                                                 + "\n"
-//                        + "    }"                                                                                                                     + "\n"
-//                        + "}"
-//        ;
-//
-//        return out;
-//    }
+
 
 
     public static String dataToCode() {
@@ -426,8 +354,8 @@ public final class Recorder {
         Format of data:
         0: nanosecond since opmode start (long)
         1: voltage                       (double)
-        2: motor powers                  (double)
-        3: servo positions               (double)
+        2 to #motors - 1 + 2: motor powers                  (double)
+        3 to #motors - 1 + #servos - 1 + 2: servo positions               (double)
     */
     public static ArrayList[] data;
 
