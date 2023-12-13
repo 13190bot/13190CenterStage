6cd855ad09e1e8b7696df857a83e8b202e8103ac
wilcoxrobotics <teamrobotics8872@gmail.com>
2023-12-13 15:56:09 -0800

    april tag loclaliser test done

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TestingTeleop/LosalisationTesting.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.TeleOp.TestingTeleop;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.BNO055IMUImpl;
+import org.firstinspires.ftc.teamcode.CV.AprilTagDetector;
+import org.firstinspires.ftc.teamcode.Localisation.*;
+import org.firstinspires.ftc.teamcode.TeleOp.MainTeleop.BaseOpMode;
+
+public class LosalisationTesting extends BaseOpMode {
+    CompositeLocalizer localizer;
+
+    @Override
+    public void initialize() {
+        super.initialize();
+
+        localizer = new CompositeLocalizer(
+                new Pose2d(),
+                new MyLocalizer[]{
+                        new IMULocaliser((BNO055IMUImpl) hardwareMap.getAll(BNO055IMU.class).get(0)),
+                        new IMULocaliser((BNO055IMUImpl) hardwareMap.getAll(BNO055IMU.class).get(1)),
+
+                        new AprilTagLocalizer(AprilTagDetector.aprilTagProcessor)
+
+//                        new OdoLocalizer()
+                }
+        );
+    }
+}
