6809780f540e39d0ab9d3f35ed1e215b93bb40e7
imaspacecat <teamrobotics8872@gmail.com>
2023-12-13 18:01:24 -0800

    W auto by neelesh - kosei
    Signed-off-by: imaspacecat <teamrobotics8872@gmail.com>

--- MeepMeepTesting/src/main/java/org/example/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/org/example/MeepMeepTesting.java
@@ -24,10 +24,22 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                       drive.trajectorySequenceBuilder(new Pose2d(10.5, -58, Math.toRadians(90)))
-                               .splineTo(new Vector2d(10.5, 10.99), Math.toRadians(0))
+                       drive.trajectorySequenceBuilder(new Pose2d(-35, -60, Math.toRadians(90)))
+
+                               .lineTo(new Vector2d(-35, -30))
+                               .splineToConstantHeading(new Vector2d(-60, -49.4), Math.toRadians(0))
+                               .splineToConstantHeading(new Vector2d(-22, -34.7), Math.toRadians(-15))
+                               .splineToConstantHeading(new Vector2d(20, -35), Math.toRadians(0))
+                               .splineToConstantHeading(new Vector2d(35, 35), Math.toRadians(90))
+
+                               //.strafeRight(57)
+                               //.forward(70)
+
+
+//                               .splineToConstantHeading(new Vector2d(46, 35), Math.toRadians(80))
                                .build()
                 );
+
 
 
 
