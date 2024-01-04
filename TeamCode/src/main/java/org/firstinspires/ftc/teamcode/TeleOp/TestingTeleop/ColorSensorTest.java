99acf56633a81b96e45fedf5db94db1663fab3d9
cryptopig <71409503+cryptopig@users.noreply.github.com>
2024-01-04 00:20:29 -0800

    changed colorsensortest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TestingTeleop/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TestingTeleop/ColorSensorTest.java
@@ -1,9 +1,11 @@
+// for testing color sensors
+
 package org.firstinspires.ftc.teamcode.TeleOp.DriveTestOpMode;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-public class DriveTestOpMode extends LinearOpMode {
+public class ColorSensorTest extends LinearOpMode {
 
 }
