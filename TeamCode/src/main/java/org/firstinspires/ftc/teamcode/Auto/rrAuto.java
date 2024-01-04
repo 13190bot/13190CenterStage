28a77b99adea3710079a0c71416587f762535873
mc08 <mc08>
2024-01-04 11:04:48 -0800

    Fixed more build issues

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/rrAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/rrAuto.java
@@ -1,11 +1,14 @@
 package org.firstinspires.ftc.teamcode.Auto;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.TeleOp.MainTeleop.BaseOpMode;
+import org.firstinspires.ftc.teamcode.util.librarys.roadrunner.drive.SampleMecanumDrive;
 
 @Autonomous(name = "Roadrunner_Auto")
-public class RoadrunnerAuto extends BaseOpMode {
+public class rrAuto extends BaseOpMode {
     @Override
     public void runOpMode() {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
