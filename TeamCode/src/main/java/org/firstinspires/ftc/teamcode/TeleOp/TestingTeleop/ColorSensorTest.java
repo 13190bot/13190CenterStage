3966e1dcc58c210ed7d826ff4a399fccff9b1454
cryptopig <71409503+cryptopig@users.noreply.github.com>
2024-01-04 00:34:17 -0800

    changed colorsensortest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TestingTeleop/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TestingTeleop/ColorSensorTest.java
@@ -5,7 +5,16 @@ package org.firstinspires.ftc.teamcode.TeleOp.DriveTestOpMode;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 
+@TeleOp(name = "Color Sensor Testing")
 public class ColorSensorTest extends LinearOpMode {
+    leftColorSensor = new ColorSensor(hardwareMap, "leftSensor");
+    rightColorSensor = new ColorSensor(hardwareMap, "rightSensor");
+    @Override
+    public void runOpMode() throws  InterruptedException {
+        telemetry.addData("Initialization:", "Initialized, variables defined.");
+        telemetry.update();
+    }
 
 }
