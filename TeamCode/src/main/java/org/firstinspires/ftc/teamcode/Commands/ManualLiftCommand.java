8754bd6f3b9f036dbd0406ceb730f23528f473e6
benjaminfaershtein <Benjaminfaer@gmail.com>
2024-01-12 10:15:12 -0800

    Manual Lift with limits

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/ManualLiftCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/ManualLiftCommand.java
@@ -21,15 +21,22 @@ public class ManualLiftCommand extends CommandBase {
             power = (gamepadEx2.getLeftY())*0.8;
 
 
+                if (liftSubsystem.checkLimits(power)) {
 
-                if (power != 0) {
-                    liftSubsystem.liftRight.set(power);
-                    liftSubsystem.liftLeft.set(-power);
+
+                    if (power != 0) {
+                        liftSubsystem.liftRight.set(power);
+                        liftSubsystem.liftLeft.set(power);
+                    } else {
+                        //Stabilize
+                        liftSubsystem.liftRight.set(0);
+                        liftSubsystem.liftLeft.set(0);
+                    }
                 } else {
-                    //Stabilize
                     liftSubsystem.liftRight.set(0);
                     liftSubsystem.liftLeft.set(0);
                 }
+
 
 
     }
