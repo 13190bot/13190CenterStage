8be78dfb30eabb0c20dc01b62d02f11d446b1f10
wilcoxrobotics <teamrobotics8872@gmail.com>
2024-01-31 15:27:54 -0800

    tuned actual to target

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/librarys/roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/librarys/roadrunner/drive/DriveConstants.java
@@ -54,8 +54,8 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.019;
-    public static double kA = 0.002;
+    public static double kV = -0.019;
+    public static double kA = -0.002;
     public static double kStatic = 0.018; // 0.23 best motion profile, but 0.18 most accurate for distance
 
     /*
